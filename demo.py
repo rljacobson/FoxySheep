2a3c6d223d6bb4c3f2470c48f29e6ec75f843eea
rocky <rb@dustyfeet.com>
2020-07-18 21:59:50 -0400

    Get demo.py working again

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@
 import os
 import os.path as osp
 import inspect
-from FoxySheep import FullForm_from_string
+from FoxySheep import input_form_to_full_form, parse_tree_from_string
 
 # IMHO this should be built-into a Python library. Perhaps it is, and I'm just not
 # aware of it.
@@ -12,11 +12,11 @@ def get_srcdir():
         osp.dirname(osp.abspath(inspect.currentframe().f_back.f_code.co_filename))
     )
 
-expression_dir_path = osp.join(get_srcdir(), "ParseExpressions")
+expression_dir_path = osp.join(get_srcdir(), "pytest", "parse_expressions")
 for mma_path in os.scandir(expression_dir_path):
     print("=" * 30, mma_path.name)
     mma_expression = open(mma_path, "r").read()
     print(mma_expression)
     print('-' * 10)
-    print(FullForm_from_string(mma_expression))
+    print(input_form_to_full_form(mma_expression, parse_tree_from_string))
     print()
