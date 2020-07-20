eeae336e88f17e07fbb0b87b40f44fb90af6b877
rocky <rb@dustyfeet.com>
2020-07-20 05:34:54 -0400

    Start putting tests in YAML...
    and DRY test program

--- pytest/test_translations.py
+++ pytest/test_translations.py
@@ -7,6 +7,18 @@ from FoxySheep.tree.pretty_printer import pretty_print_string
 from FoxySheep.emitter.full_form import input_form_to_full_form
 from FoxySheep.transform.if2py import input_form_to_python
 
+from typing import Callable
+import yaml
+import os.path as osp
+
+def get_srcdir():
+    filename = osp.normcase(osp.dirname(osp.abspath(__file__)))
+    return osp.realpath(filename)
+
+
+srcdir = get_srcdir()
+testdata_dir = osp.join(srcdir, "parse_expressions")
+
 last_tree_str = ""
 
 def parse_tree_fn(expr: str, show_tree_fn):
@@ -20,30 +32,32 @@ def parse_tree_fn(expr: str, show_tree_fn):
 
 pp_fn = lambda tree, rule_names: pretty_print_string(tree, rule_names, compact=True)
 
+show_tests=True
 
-def test_numberLiteral():
+def do_test(input_base: str, translation_fn: Callable):
+    testdata_path = osp.join(testdata_dir, "input2full.yaml")
+    with open(testdata_path, "r") as yaml_file:
+        test_data = yaml.load(yaml_file, Loader=yaml.FullLoader)
+        # print(test_data)
 
-    for expr, tree_str_expect in (
-        ("1", "(prog (expr (numberLiteral '1')))"),
-        ("12", "(prog (expr (numberLiteral '12')))"),
-        ("167.5", "(prog (expr (numberLiteral '167.5')))"),
-        ("15^^8", "(prog (expr (numberLiteral '15' '^^8')))"),
-        ("55*^2", "(prog (expr (numberLiteral '55' (numberLiteralExponent '*^' '2'))))"),
-        # ("10^^3.58.3", "(expr (numberLiteral '55' '^^8.3')))"),
-        ("132`", "(prog (expr (numberLiteral '132' (numberLiteralPrecision '`'))))"),
-        (
-            "167^^8``5",
-            "(prog (expr (numberLiteral '167' '^^8' (numberLiteralPrecision '``' '5'))))",
-        ),
-    ):
-        s = input_form_to_full_form(expr, parse_tree_fn, pp_fn)
-        # print(s)
-        # print("-" * 30)
-        # print(last_tree_str)
-        # print("=" * 30)
+    for section, test_list in test_data.items():
+        print(f'{"*" * 10} {section} {"*" * 10}')
+        for test_item in test_list:
+            expr = str(test_item["InputForm"])
+            tree_str_expect = test_item["tree"]
+            full_form_expect = str(test_item.get("FullForm", expr))
+            s = translation_fn(expr, parse_tree_fn, pp_fn)
+            if show_tests:
+                print(s)
+                print(full_form_expect)
+                print(last_tree_str)
+                print("=" * 30)
 
-        assert expr == s
-        assert last_tree_str == tree_str_expect
+            assert s == full_form_expect
+            assert last_tree_str == tree_str_expect
+
+def test_FullForm():
+    do_test("input2full.yaml", input_form_to_full_form)
 
 def test_numberLiteral2Py():
 
@@ -68,30 +82,6 @@ def test_numberLiteral2Py():
 
         assert output_expect == s.strip()
         assert last_tree_str == tree_str_expect
-
-def test_binOp():
-
-    for expr, tree_str_expect, full_form_expect in (
-        (
-            "1 ** 10",
-            "(prog (expr (expr (numberLiteral '1')) '**' (expr (numberLiteral '10'))))",
-            "NonCommutativeMultiply[1,10]",
-        ),
-        (
-            "1 / 10 3",
-            "(prog (expr (expr (expr (numberLiteral '1')) '/' (expr (numberLiteral '10'))) (expr (numberLiteral '3'))))",
-            "Times[Times[1,Power[10,-1]],3]",
-        ),
-    ):
-        s = input_form_to_full_form(expr, parse_tree_fn, pp_fn)
-        # print(s)
-        # print("-" * 30)
-        # print(last_tree_str)
-        # print("=" * 30)
-
-        assert s == full_form_expect
-        assert last_tree_str == tree_str_expect
-
 
 def test_binOp2Py():
 
@@ -118,4 +108,4 @@ def test_binOp2Py():
 
 
 if __name__ == "__main__":
-    test_numberLiteral2Py()
+    test_FullForm()
